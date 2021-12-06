14782a443e17619b03676a27e9338c92a57a84ec
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2021-12-06 11:00:00 +0200

    Document race conditions handled by WorkerState and add restart notification

--- src/WorkerState.h
+++ src/WorkerState.h
@@ -5,6 +5,40 @@
 #include <condition_variable>
 #include <mutex>
 
+// WorkerState: tracks whether a worker thread should continue running.
+//
+// Approach
+//   An atomic flag indicates running/stopped state. A condition variable
+//   allows workers to sleep until explicitly stopped or until they need
+//   to wake for other reasons (e.g. new work available).
+//
+// Complexity
+//   start/stop      O(1)   atomic store + notify_all
+//   is_running      O(1)   atomic load
+//   wait_for_stop   O(1)   condition_variable wait with predicate
+//
+// Thread safety: all methods are safe to call from any thread.
+// stop() is idempotent.
+//
+// Race conditions handled:
+//   1. Concurrent stop() calls: atomic flag ensures only one transition
+//   2. stop() during wait_for_stop(): notify_all wakes all waiters
+//   3. is_running() check before wait: predicate rechecks after wake
+//   4. stop() before worker starts: initial state check prevents entry
+//   5. Multiple workers: each checks its own state independently
+//
+// Usage pattern:
+//   WorkerState state;
+//   std::thread worker([&] {
+//       while (state.is_running()) {
+//           // do work
+//           state.wait_for_stop(timeout);
+//       }
+//   });
+//   // later...
+//   state.stop();
+//   worker.join();
+
 class WorkerState
 {
 public:
@@ -34,8 +68,11 @@ public:
 
     void restart()
     {
-        std::lock_guard<std::mutex> lock(_mtx);
-        _running.store(true, std::memory_order_release);
+        {
+            std::lock_guard<std::mutex> lock(_mtx);
+            _running.store(true, std::memory_order_release);
+        }
+        _cv.notify_all();
     }
 
 private:
